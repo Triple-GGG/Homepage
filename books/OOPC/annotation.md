b6fd9bb52a38ad7dbb5c377ded00941e3a4e9aa3
Triple-GGG <3431714864@qq.com>
2017-12-13 09:52:42 +0800

    chapter 3

--- books/OOPC/annotation.md
+++ books/OOPC/annotation.md
@@ -1,1 +1,10 @@
 # Annotation
+<!-- TOC depthFrom:2 depthTo:6 withLinks:1 updateOnSave:1 orderedList:0 -->
+
+- [Chapter 3 Programming Savvy -- Arithmetic Expressions](#chapter-3-programming-savvy-arithmetic-expressions)
+
+<!-- /TOC -->
+
+## Chapter 3 Programming Savvy -- Arithmetic Expressions
+
+This chapter illustrates the benefit of dynamic linkage through writing a program to read and evaluate arithmetic expressions. See []().
--- books/OOPC/src/ch03/main.c
+++ books/OOPC/src/ch03/main.c
@@ -1,8 +1,12 @@
 #include <setjmp.h>
+#include <stdarg.h>
+#include <stdio.h>
 
 static enum tokens token;
 
 static jmp_buf onError;
+
+void error(const char *fmt, ...);
 
 int main() {
     volatile int errors = 0;
@@ -21,4 +25,13 @@ int main() {
         }
 
     return errors > 0;
+}
+
+void error(const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    putc('\n', stderr);
+    va_end(ap);
+    longjmp(onError, 1);
 }
